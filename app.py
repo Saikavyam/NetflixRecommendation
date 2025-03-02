a6142eb7b83f0ef8d189f9802e8a733e4932461e
Saikavyam <157380494+Saikavyam@users.noreply.github.com>
2025-03-02 21:42:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 movies_df = pd.read_csv(os.path.join(BASE_DIR, 'movies_df.csv'))
 tv_show = pd.read_csv(os.path.join(BASE_DIR, 'tv_show.csv'))
-movies_sim = np.load(os.path.join(BASE_DIR, 'movies_sim.npz'))['m']
+movies_sim = np.load(os.path.join(BASE_DIR, 'movies_sim.npz.7z'))['m']
 tv_sim = np.load(os.path.join(BASE_DIR, 'tv_sim.npz'))['t']
 # Recommendation Function
 def recommend(title):
